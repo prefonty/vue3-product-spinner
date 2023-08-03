ad5f01db69ad147d71417c6dcef2be1a8f7bc637
yanik <yanik@cybercat.ca>
2023-08-03 15:24:50 -0400

    demo deploy

--- build-demo.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-git add demo && git commit -m "demo deploy"
-git subtree push --prefix demo origin gh-pages
--- src/index.js
+++
@@ -1,4 +0,0 @@
-import Vue from "vue";
-import VueProductSpinner from "./components/VueProductSpinner.vue";
-
-export default Vue.component("VueProductSpinner", VueProductSpinner);
